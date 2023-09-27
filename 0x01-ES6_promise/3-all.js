4af14b39820bee93335723a5c1fa79bfc18da0e2
SamZec <samzec4@gmail.com>
2023-09-27 21:06:54 +0000

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,7 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((values) => {
     const log = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
     console.log(log);
-  });
+  },
+  (error) => console.log("Signup system offline");
+  );
 }
