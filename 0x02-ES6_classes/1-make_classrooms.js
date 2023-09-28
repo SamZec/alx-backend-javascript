0a93cedf2b3edd6145ba5a8377e002ca6b844a98
SamZec <samzec4@gmail.com>
2023-09-28 23:05:10 +0000

    1. Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,8 +3,10 @@ import ClassRoom from './0-classroom.js';
 export default function initializeRooms() {
   let array = [];
   const sizes = [19, 20, 34];
+
   for (const item of sizes) {
     array.push(new ClassRoom(item));
   }
+
   return array;
 }
