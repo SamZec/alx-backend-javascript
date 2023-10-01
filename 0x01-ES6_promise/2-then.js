c6d05b49944e879198ea56e46ee6810e4cd66db7
SamZec <samzec4@gmail.com>
2023-10-01 06:18:52 +0000

    Correct errors

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,16 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then((value) => {
-    value = {
+    return {
+      value: value,
       'status': 200,
       body: 'success',
     };
     console.log('Got a response from the API');
   },
-  (error) => {error = new Error('hhh')});
+  (err) => {
+    return { 
+      err: err,
+      error: new Error(),
+    };
+  });
 }
