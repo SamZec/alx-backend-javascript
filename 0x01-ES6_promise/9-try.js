13295006c3db40d19fbd0040e17cff6c5b3a7699
SamZec <samzec4@gmail.com>
2023-10-01 07:26:51 +0000

    Correct errors

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,14 @@
 export default function guardrail(mathFunction) {
-  const array = [];
+  const queue = [];
   try {
     const res = mathFunction();
-    array.push(res);
-    array.push('Guardrail was processed');
+    queue.push(res);
+    queue.push('Guardrail was processed');
   }
+
   catch(error) {
-    array.push(error);
-    array.push('Guardrail was processed');
+    queue.push(error);
+    queue.push('Guardrail was processed');
   }
-  return array;
+  return queue;
 }
