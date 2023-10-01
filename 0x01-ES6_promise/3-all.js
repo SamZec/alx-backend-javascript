4058932a35cd01fb6cea16e5fc618d4392d5ffbb
SamZec <samzec4@gmail.com>
2023-10-01 06:56:17 +0000

    Correct errors

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,15 @@
-import {uploadPhoto, createUser} from '../utils.js';
+import { uploadPhoto, createUser } from '../utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    const log = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
-    console.log(log);
-  }).catch((error) => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const log = `${values[0].body} ${values[1].firstName} ${values[1].lastName}`;
+      console.log(log);
+  })
+    .catch((error) => {
+    if (error) {
+      console.log('Signup system offline');
+    }
+    return error;
+    });
 }
