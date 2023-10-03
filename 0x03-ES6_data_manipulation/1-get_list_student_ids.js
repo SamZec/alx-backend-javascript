43414c080a3a4619ec8c811d30aecd1fdbebfd81
SamZec <samzec4@gmail.com>
2023-10-03 18:08:48 +0000

    1. More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
 export default function getListStudentIds(getListStudents) {
-  let array = new Array;
   if (getListStudents.constructor === Array) {
-    array = getListStudents.map((object) => object.id);
-    return array;
+    return getListStudents.map((object) => object.id);
   }
-  return array;
+  return new Array();
 }
