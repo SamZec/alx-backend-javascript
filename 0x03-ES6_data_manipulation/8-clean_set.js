8f6f12a26a9d1c1c2fb6aa5ef57e0adc12474de5
SamZec <samzec4@gmail.com>
2023-10-03 22:39:43 +0000

    8. Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  let string = '';
+  if (startString === '' || typeof startString !== 'string') {
+    return string;
+  }
+  set.forEach((value) => {
+    if (value.match(startString)) {
+       string += value.slice(startString.length, (value.length + 1));
+       string += '-';
+    }
+  });
+  return string.slice(0, - 1);
+}
