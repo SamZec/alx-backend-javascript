813b326254900038bd52050545fdd02822520565
SamZec <samzec4@gmail.com>
2023-10-04 08:09:41 +0000

    Correct output and style errors

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(getListStudents) {
-  let ids = []
+  let ids = [];
   if (getListStudents.constructor === Array) {
     ids = getListStudents.map((object) => object.id);
   }
