1750fa1612fabcb47c825bdc081a5d6b0b9233f0
SamZec <samzec4@gmail.com>
2023-10-04 09:24:18 +0000

    Correct output and style errors

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,12 +4,13 @@ function queryAPI(endpoint) {
   let counter = 0;
   if (weakMap.has(endpoint)) {
     counter = weakMap.get(endpoint);
-    if (counter >= 5) {
+    if (counter + 1 >= 5) {
+      weakMap.set(endpoint, counter + 1);
+    } else {
       throw new Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, ++counter);
   } else {
-    weakMap.set(endpoint, ++counter);
+    weakMap.set(endpoint, counter + 1);
   }
   return endpoint;
 }
