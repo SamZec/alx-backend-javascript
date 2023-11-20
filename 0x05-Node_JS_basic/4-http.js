b47f2dc77e5db7ba6e8746e956cff386977e117a
SamZec <samzec4@gmail.com>
2023-11-20 21:46:18 +0000

    4. Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+app = require('http');
+
+
+const server = app.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+server.listen(1245, 'localhost');
