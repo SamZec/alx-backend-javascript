dc772a88b11dbb8622d5c95cccd87cff38d339c0
SamZec <samzec4@gmail.com>
2023-11-21 15:45:33 +0000

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  let data = ''
+  let data = '';
   try {
     data = fs.readFileSync(path, 'utf8');
   }
@@ -10,18 +10,18 @@ module.exports = function countStudents(path) {
     throw new Error('Cannot load the database');
   }
   const line = data.split('\n');
-  const list = line.slice(1, -1);
+  const list = line.slice();
   const obj = {CS: [], SWE: []};
   for (let data of list) {
     if (data.includes('CS')) {
       const value = data.split(',').slice(0);
       obj.CS.push(' ' + value[0]);
-    } else {
+    } else if (data.includes('SWE')) {
       const value = data.split(',').slice(0);
       obj.SWE.push(' ' + value[0]);
     }
   }
-  const num = obj.SWE.length + obj.CS.length
+  const num = obj.SWE.length + obj.CS.length;
   console.log(`Number of students: ${num}`);
   console.log(`Number of students in CS: ${obj.CS.length}. List: ${obj.CS}`);
   console.log(`Number of students in SWE: ${obj.SWE.length}. List: ${obj.SWE}`);
