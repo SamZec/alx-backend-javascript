4b2e4039a42573546e35762a67b2c5c22b539017
SamZec <samzec4@gmail.com>
2023-11-25 11:07:30 +0000

    Update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,8 +2,18 @@ const calculateNumber = require("./0-calcul.js");
 const assert = require('assert');
 
 describe("calculateNumber test", function() {
-  it("checks rounded", function() {
-    assert.equal(calculateNumber(1, 3), 4);
+  it("it round the first argument", function() {
+    assert.equal(calculateNumber(1.1, 0), 1);
+    assert.equal(calculateNumber(0.5, 0), 1);
+    assert.equal(calculateNumber(2.1, 0), 2);
+  });
+});
+
+describe("calculateNumber test", function() {
+  it("it round the second argument", function() {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 0.5), 1);
+    assert.equal(calculateNumber(0, 2.1), 2);
   });
 });
 
