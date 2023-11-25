28a0eb9e0fbaff60e3caac49ec6c072116788b4a
SamZec <samzec4@gmail.com>
2023-11-25 16:19:18 +0000

    5. Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const { ShouldThrow, expect } = require('chai');
+const sinon = require('sinon');
+
+const sendPaymentRequestToAPI = require('./5-payment.js');
+
+describe('sendPaymentRequestToAPI Test', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('logs to the console the right messages', () => {
+    sendPaymentRequestToAPI(100, 20);
+
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+
+    sendPaymentRequestToAPI(10, 10);
+
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+    expect(consoleSpy.calledOnce).to.be.true;
+  });
+});
