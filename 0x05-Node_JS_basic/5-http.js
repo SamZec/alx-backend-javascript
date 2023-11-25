a310ce01ddc62fa9ed3307a3215acc3919c21cb6
SamZec <samzec4@gmail.com>
2023-11-25 08:13:37 +0000

    Update

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,64 +1,57 @@
-const fs = require('fs');
-const util = require('util');
 const http = require('http');
+const fs = require('fs');
+const { argv } = require('process');
 
-const readFile = util.promisify(fs.readFile);
+function countStudents(path, stream) {
+  if (fs.existsSync(path)) {
+    const data = fs.readFileSync(path, 'utf8');
+    const result = [];
+    data.split('\n').slice(0, -1).forEach((data) => {
+      result.push(data.split(','));
+    });
+    result.shift();
+    const newis = [];
+    result.forEach((data) => newis.push([data[0], data[3]]));
+    const fields = new Set();
+    newis.forEach((item) => fields.add(item[1]));
+    const final = {};
+    fields.forEach((data) => { (final[data] = 0); });
+    newis.forEach((data) => { (final[data[1]] += 1); });
+    stream.write(`Number of students: ${result.length}\n`);
+    const temp = [];
+    Object.keys(final).forEach((data) => temp.push(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < temp.length; i++) {
+      if (i === temp.length - 1) {
+        temp[i] = temp[i].replace(/(\r\n|\n|\r)/gm, '');
+      }
+      stream.write(temp[i]);
+    }
+  } else { throw new Error('Cannot load the database'); }
+}
 
-function countStudents(path) {
-  let data = '';
-  try {
-    data = readFile(path, 'utf8');
-  }
-  catch(err) {
-    err.message = 'Cannot load the database';
-    throw new Error('Cannot load the database');
-  }
-  return data;
-};
+const hostname = 'localhost';
+const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  switch (req.url) {
-    case '/students':
-      try {
-	res.write('This is the list of our students\n');
-        countStudents(process.argv[2]).then((items) => {
-	  const line = items.split('\n');
-	  const list = line.slice();
-	  const obj = {CS: [], SWE: []};
-	  for (let li of list) {
-	    if (li.includes('CS')) {
-	      const value = li.split(',').slice(0);
-	      obj.CS.push(' ' + value[0]);
-	    } else if (li.includes('SWE')) {
-	      const value = li.split(',').slice(0);
-	      obj.SWE.push(' ' + value[0]);
-	    }
-	  }
-	  const SWE = obj.SWE.length;
-	  const CS = obj.CS.length;
-	  const total = SWE + CS;
-	  res.write(`Number of students: ${total}\n`);
-	  res.write(`Number of students in CS: ${CS}. List: ${obj.CS}\n`);
-	  res.write(`Number of students in SWE: ${SWE}. List: ${obj.SWE}`);
-	res.end();
-	})
-	.catch((err) => {
-	  res.write(err.message);
-	  res.end();
-	});
-      }
-      catch(err) {
-        res.write(err.message);
-	res.end();
-      }
-      break
-    default:
-      res.end('Hello Holberton School!');
+  const { url } = req;
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      countStudents(argv[2], res);
+      res.end();
+    } catch (err) {
+      res.end(err.message);
+    }
   }
 });
 
-app.listen(1245, 'localhost');
+app.listen(port, hostname);
 
 module.exports = app;
