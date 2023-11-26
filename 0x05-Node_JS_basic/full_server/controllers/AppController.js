07926d1dc7e9edd36ab52427d2c43e80abe5b4e9
SamZec <samzec4@gmail.com>
2023-11-26 11:59:20 +0000

    8. Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,9 @@
+class AppController {
+  static getHomepage(request, response) {
+    response.statusCode = 200;
+    response.end('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
