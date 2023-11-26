402eb387e59dfe0cb7ed5b77da1cba0ab33745ca
Samuel Zachariah Kwabena Affum <samzec4@gmail.com>
2023-11-26 06:10:20 +0000

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,14 +2,16 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('index page', function () {
-  it('checks correct API status code', function () {
+  it('checks correct API status code', function (done) {
     request('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
+      done();
     });
   });
-  it('GET / gives correct API response', function () {
+  it('GET / gives correct API response', function (done) {
     request('http://localhost:7865', (err, res, body) => {
       expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
   });
 });
