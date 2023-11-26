4d0a90c5dc39c1d3bb313b76a0e4fabdc7d7210e
Samuel Zachariah Kwabena Affum <samzec4@gmail.com>
2023-11-26 06:00:21 +0000

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,5 +1,4 @@
 const { expect } = require('chai');
-const sinon = require('sinon')
 const request = require('request');
 
 describe('index page', function () {
@@ -12,9 +11,5 @@ describe('index page', function () {
     request('http://localhost:7865', (err, res, body) => {
       expect(body).to.be.equal('Welcome to the payment system');
     });
-  });
-  it('checks proper logging message on console', function () {
-    consoleSpy = sinon.spy(console, 'log');
-    expect(consoleSpy.calledWith('API available on localhost port 7865')).to.be.true;
   });
 });
